18936fd240d0b0e95ce9ddca48c10786affb6432
Ömer  ÖZTÜRK <omer.ozturk.or.ok@gmail.com>
2024-08-06 14:50:27 +0300

    Single Responsibility Principle

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Solid 
-# Solid 
+# Solid
---
+++ SRP/SRP/IUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    internal interface IUser
+    {
+        bool Login(string userName, string password);
+        bool Register(string userName, string password, string email);
+
+        /*
+         No SRP avaliable useage
+
+         void LogError(string errorMessage);
+         bool SendEmail(string emailContent);
+        */
+    }
+
+    // SRP avaliable useage
+
+    interface ILogger
+    {
+        void LogError(string errorMessage);
+    }
+    interface IEmail
+    {
+        bool SendEmail(string emailContent);
+    }
+}
